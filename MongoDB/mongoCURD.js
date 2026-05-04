5398c89166c767ed381838cc7925e3e977c30051
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-05-04 13:13:26 +0530

    MongoDB, mongoose

--- MongoDB/mongoCURD.js
+++ MongoDB/mongoCURD.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 mongoose.connect("mongodb://localhost:27017/student");
+let app = express();
 
 let studentSchema = mongoose.Schema({
     name: String,
@@ -12,4 +13,26 @@ let studentSchema = mongoose.Schema({
 
 let Student = mongoose.model("student", studentSchema);
 
+app.use(express.json());
 
+app.post("/students", async(req, res)=>{
+    let result = await Student.insertOne(req.body);
+    res.json(result);
+})
+
+app.put("/students/:id", async(req, res)=>{
+    let result = await Student.updateOne({_id:req.params.id}, {$set:req.body});
+    res.json(result);
+})
+
+app.delete("/students/:id", async(req, res)=>{
+    let result = await Student.deleteOne({_id:req.params.id});
+    res.json(result);
+})
+
+app.get("/students", async(req, res)=>{
+    let result = await Student.find();
+    res.json(result);
+})
+
+app.listen(3000);
